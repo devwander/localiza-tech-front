e624e5f0e4ffa57d94590ed529dbf6a0467b1870
devwander <josewanderson173@gmail.com>
2025-12-02 17:42:49 -0300

    fix: Correct import order in sign-in form component

--- src/pages/auth/sign-in/form.tsx
+++ src/pages/auth/sign-in/form.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useSigninMutation } from "../../../mutation";
